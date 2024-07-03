a7d2cfabd2298b47666870ba4c59d65cefa9ea9d
lavodky.dev <s.1dn3y@hotmail.com>
2024-07-03 15:03:52 -0300

    fix from

--- js/main.js
+++ js/main.js
@@ -93,7 +93,7 @@
       if ($data_filters == 'all') {
         $portfolio_grid_item.addClass('visible');
       }
-      else { // Show Portfolio Items From filter
+      else { // Show Portfolio Items from filter
         $portfolio_grid_item.removeClass('visible');
         $($data_filters).addClass('visible');
       }
@@ -126,7 +126,7 @@
       // Hide Overlay Image
       $(this).fadeOut(200);
 
-      // Remove Image From DOM
+      // Remove Image from DOM
       $overlay.children("img").remove();
 
       // Unlock Body Scroll
